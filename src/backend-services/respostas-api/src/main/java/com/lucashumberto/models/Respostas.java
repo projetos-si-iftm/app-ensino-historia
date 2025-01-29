a6ba92f229cb4d6176fd82b889afa74df2d4395c
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2025-01-28 21:51:12 -0300

    Esqueleto inicial serviço respostas e estatisticas

---
+++ src/backend-services/respostas-api/src/main/java/com/lucashumberto/models/Respostas.java
@@ -0,0 +1,16 @@
+package com.lucashumberto.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Respostas {
+    @Id
+    private String id;
+
+    private String alunoId;
+
+    private String questaoId;
+
+    private String alternativaId;
+
+    private 
+}
---
+++ src/backend-services/respostas-api/src/main/java/com/lucashumberto/respostas_api/RespostasApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucashumberto.respostas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespostasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespostasApiApplication.class, args);
+	}
+
+}
