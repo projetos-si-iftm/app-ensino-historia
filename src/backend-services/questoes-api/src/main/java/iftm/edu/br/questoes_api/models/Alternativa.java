10adf84c930e515ca9b984bd6c9191f93a80811a
william.alves <alvesrez@gmail.com>
2025-01-29 23:49:42 -0300

    correção do código para Mapear o campo correto do banco de dados

--- src/backend-services/questoes-api/src/main/java/iftm/edu/br/questoes_api/models/Alternativa.java
+++ src/backend-services/questoes-api/src/main/java/iftm/edu/br/questoes_api/models/Alternativa.java
@@ -3,6 +3,8 @@ package iftm.edu.br.questoes_api.models;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "alternativas")
+@Document(collection = "alternativas") // Mapeia a coleção do MongoDB
 public class Alternativa {
     @Id
     private String id;
     private String texto;
+
+
+    @Field("correto") // Mapeia o campo correto do banco de dados
     private boolean isCorreto;
     private LocalDateTime dataCriacao;
     private LocalDateTime dataAtualizacao;
