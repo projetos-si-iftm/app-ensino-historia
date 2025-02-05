186c61b382fc5fd6ac07c0cf88caf64b7e561400
Natalia Arantes <natalia.arantes@estudante.iftm.edu.br>
2025-02-05 13:55:50 -0300

    Serviço de autênticação: endpoint de validação de token

--- auth/src/main/java/com/nataliaarantes/iftm/controller/AuthorizationController.java
+++ auth/src/main/java/com/nataliaarantes/iftm/controller/AuthorizationController.java
@@ -1,12 +1,13 @@
 package com.nataliaarantes.iftm.controller;
 
 
+import com.nataliaarantes.iftm.model.dto.ValidateTokenDTO;
 import com.nataliaarantes.iftm.model.dto.login.LoginDTO;
 import com.nataliaarantes.iftm.model.dto.login.LoginResponseDTO;
 import com.nataliaarantes.iftm.model.dto.register.RegisterDTO;
 import com.nataliaarantes.iftm.model.dto.register.RegisterResponseDTO;
 import com.nataliaarantes.iftm.service.AuthorizationService;
-import com.nataliaarantes.iftm.service.SecurityAuthorizationService;
+import com.nataliaarantes.iftm.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("auth")
 public class AuthorizationController {
 
   @Autowired
   private AuthorizationService authorizationService;
+
+  @Autowired
+  private TokenService tokenService;
 
 
   @PostMapping("/login")
@@ -33,5 +39,13 @@ public class AuthorizationController {
   public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterDTO requestBody){
     var response = authorizationService.register(requestBody);
     return new ResponseEntity<>(response, HttpStatusCode.valueOf(201));
+  }
+
+  @PostMapping("/validateToken")
+  public ResponseEntity<Boolean> validateToken(@RequestBody ValidateTokenDTO validateTokenDTO) {
+    var response = tokenService.validateToken(validateTokenDTO.getToken());
+    var isValid = !Objects.equals(response, "");
+
+    return new ResponseEntity<>(isValid, HttpStatusCode.valueOf(200));
   }
 }
