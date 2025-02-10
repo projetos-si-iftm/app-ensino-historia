418417d6f25bff34207cdad2a4df25ba68796c38
Natalia Arantes <natalia.arantes@estudante.iftm.edu.br>
2025-02-10 09:58:20 -0300

    Serviço de autênticação: Swagger

--- auth/src/main/java/com/nataliaarantes/iftm/config/SecurityConfig.java
+++ auth/src/main/java/com/nataliaarantes/iftm/config/SecurityConfig.java
@@ -28,6 +28,10 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.GET, "/v3/api-docs/swagger-config").permitAll()
+            .requestMatchers(HttpMethod.GET, "/v3/api-docs").permitAll()
+            .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/validateToken").permitAll()
