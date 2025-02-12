e43c92e6f689e2729f928d47779c4295391ed2c4
Natalia Arantes <natalia.arantes@estudante.iftm.edu.br>
2025-02-12 11:01:45 -0300

    Serviço de autênticação: Testes automatizados

--- auth/src/main/java/com/nataliaarantes/iftm/model/dto/login/LoginDTO.java
+++ auth/src/main/java/com/nataliaarantes/iftm/model/dto/login/LoginDTO.java
@@ -1,8 +1,10 @@
 package com.nataliaarantes.iftm.model.dto.login;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginDTO {
   private String email;
   private String password;
