5918232fdfd5eb45c3ac87916d7216d610442a3f
matuzalembatista <matuzalem_pereira@hotmail.com>
2025-02-18 19:53:50 -0300

    correcao de importe e pom do gateway

--- src/backend-services/gateway-api/src/main/java/com/iftm/gateway_api/GatewayApiApplication.java
+++ src/backend-services/gateway-api/src/main/java/com/iftm/gateway_api/GatewayApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
 
 @SpringBootApplication
 public class GatewayApiApplication {
@@ -12,6 +13,7 @@ public class GatewayApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApiApplication.class, args);
     }
+
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
