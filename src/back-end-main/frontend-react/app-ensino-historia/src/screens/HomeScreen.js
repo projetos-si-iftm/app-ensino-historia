aae33f9d4caab5d85f1994ef0fdb08e962e240e1
william.alves <alvesrez@gmail.com>
2025-05-20 20:27:42 -0300

    corrigido o local host

--- src/back-end-main/frontend-react/app-ensino-historia/src/screens/HomeScreen.js
+++ src/back-end-main/frontend-react/app-ensino-historia/src/screens/HomeScreen.js
@@ -35,7 +35,7 @@ const HomeScreen = () => {
                     title: item.descricao,
                     //level: 'Iniciante', // Ajuste conforme necessário
                     //questions: 10, // Ajuste conforme necessário
-                    image: "https://humanidades.com/wp-content/uploads/2018/09/astronomia-1-e1580089616804.jpg"
+                     image: { uri: `http://192.168.1.10:8082/uploads/${item.dados}` },
                 }));
 
                 setThemes(mappedThemes);
