b439476a205c17fd0dfea5516936749c0ed775ae
william.alves <alvesrez@gmail.com>
2025-05-26 23:47:25 -0300

    coreção nome questtão

--- src/back-end-main/questoes-api/src/test/java/iftm/edu/br/questoes_api/QuestaoApplicationTests.java
+++ src/back-end-main/questoes-api/src/test/java/iftm/edu/br/questoes_api/QuestaoApplicationTests.java
@@ -1,4 +1,4 @@
-package iftm.edu.br.questoes-api;
+package iftm.edu.br.questoes_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
